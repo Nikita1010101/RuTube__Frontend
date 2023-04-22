23b51fc0a1683fa7e009078573faae66dfb4d982
Nikita <Arzomasovitcher@gmail.com>
2023-04-22 15:25:02 +0500

    Upload

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-export const API_URL = 'https://641ece07ad55ae01ccb02699.mockapi.io/api'
-
 export const axiosClassic = axios.create({
-	baseURL: API_URL
+	baseURL: process.env.API_URL
 })
